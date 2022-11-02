d44d184304b2db06a2213b2f48565f4f087adcbe
admin <admin@DESKTOP-QBKRNN2>
2022-11-02 16:35:16 +0530

    Frist-commit-1

---
+++ src/basic/demo1.java
@@ -0,0 +1,32 @@
+package basic;
+
+public class demo1 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("good morning");
+		
+		
+		m1();
+		
+		
+	}
+	
+	public static void m1() 
+	{
+		System.out.println("hiii");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
